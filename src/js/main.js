16c7c4bac444468d616075a96d7f8dd63491c972
신나현 <nahyeon.shin@hnine.com>
2025-10-28 09:34:01 +0900

    다크모드 기능 활성화

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 import "@scss/style.scss";
 
-// const elDarkmodeToggleButton = document.querySelector("#btnDarkmode");
+const elDarkmodeToggleButton = document.querySelector("#btnDarkmode");
 
-// let colorThemeIsDark = false;
-// elDarkmodeToggleButton.addEventListener("click", () => {
-// 	colorThemeIsDark = !colorThemeIsDark;
-// 	elDarkmodeToggleButton.classList.toggle("on");
-// 	if (colorThemeIsDark)
-// 		document.documentElement.setAttribute("data-color-theme", "dark");
-// 	else document.documentElement.setAttribute("data-color-theme", "light");
-// });
+let colorThemeIsDark = false;
+elDarkmodeToggleButton.addEventListener("click", () => {
+	colorThemeIsDark = !colorThemeIsDark;
+	elDarkmodeToggleButton.classList.toggle("on");
+	if (colorThemeIsDark)
+		document.documentElement.setAttribute("data-color-theme", "dark");
+	else document.documentElement.setAttribute("data-color-theme", "light");
+});
